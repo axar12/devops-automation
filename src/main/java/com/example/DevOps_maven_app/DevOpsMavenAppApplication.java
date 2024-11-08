6868c6ed5101c888135168280d2549af16246a27
Akshar Patel <apate900@my.centennialcollege.ca>
2024-11-07 19:28:39 -0500

    first commit

---
+++ src/main/java/com/example/DevOps_maven_app/DevOpsMavenAppApplication.java
@@ -0,0 +1,14 @@
+package com.example.DevOps_maven_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class DevOpsMavenAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsMavenAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/DevOps_maven_app/DevOpsMavenAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.DevOps_maven_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevOpsMavenAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
